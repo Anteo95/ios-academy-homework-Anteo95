b1f353fc3dbdc9a5fe2068c121ea1e265e036a6a
Anteo Ivankov <anteo.ivankov@gmail.com>
2019-07-05 15:50:51 +0200

    Added Tap button and label on Login Screen.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2019 Anteo Ivankov. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var tapCount = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func onBtnTap(_ sender: Any) {
+        tapCount += 1
+        label.text = String(tapCount)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
