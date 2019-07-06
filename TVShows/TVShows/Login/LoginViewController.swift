935cdbc9eeaff5bd8b5d09a7a3da895e510785da
Anteo Ivankov <anteo.ivankov@gmail.com>
2019-07-06 09:34:26 +0200

    Changed Appearance of Login screen and added activity indicator. Added functionality to start/stop activity indicator.

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -11,16 +11,36 @@ import UIKit
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var button: UIButton!
     
     var tapCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        button.layer.cornerRadius = button.frame.height / 2
+        
+        activityIndicator.startAnimating()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { [weak self] in
+            guard let indicator = self?.activityIndicator else { return }
+            if indicator.isAnimating {
+                indicator.stopAnimating()
+                self?.button.setTitle("Start!", for: UIControl.State.normal)
+            }
+        }
     }
     
     @IBAction func onBtnTap(_ sender: Any) {
+        if(activityIndicator.isAnimating) {
+            activityIndicator.stopAnimating()
+            button.setTitle("Start!", for:UIControl.State.normal)
+        } else {
+            activityIndicator.startAnimating()
+            button.setTitle("Stop!", for: UIControl.State.normal)
+        }
         tapCount += 1
-        label.text = String(tapCount)
+        label.text = "Tap count: \(tapCount)"
     }
     
     /*
