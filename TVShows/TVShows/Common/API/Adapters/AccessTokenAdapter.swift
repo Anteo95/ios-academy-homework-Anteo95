944700d024daf06bb0ef12a39e225ae8257f9ecc
Anteo Ivankov <anteo.ivankov@gmail.com>
2019-07-19 17:19:50 +0200

    Added AccessTokenAdapter class which is used for attaching token to API requests.

---
+++ TVShows/TVShows/Common/API/Adapters/AccessTokenAdapter.swift
@@ -0,0 +1,31 @@
+//
+//  AccessTokenAdapter.swift
+//  TVShows
+//
+//  Created by Anteo Ivankov on 18/07/2019.
+//  Copyright © 2019 Anteo Ivankov. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+final class AccessTokenAdapter: RequestAdapter {
+    
+    // MARK: - Private properties
+    
+    private let accessToken: String
+
+    // MARK: - Initialization
+    
+    init(accessToken: String) {
+        self.accessToken = accessToken
+    }
+
+    // MARK: - RequestAdapter
+    
+    func adapt(_ urlRequest: URLRequest) throws -> URLRequest {
+        var urlRequest = urlRequest
+        urlRequest.setValue("Bearer " + accessToken, forHTTPHeaderField: "Authorization")
+        return urlRequest
+    }
+}
